3988fb5139c7b6a0be0050d3ecf908b4486c42e0
triple <dyd002700@gmail.com>
2023-04-14 17:51:57 +0900

    엔티티설계3

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -7,6 +7,11 @@ import javax.persistence.Embeddable;
 @Embeddable
 @Getter
 public class Address {
+        public Address(String city, String street, String zipcode) {
+                this.city = city;
+                this.street = street;
+                this.zipcode = zipcode;
+        }
 
         private String city;
         private String street;
